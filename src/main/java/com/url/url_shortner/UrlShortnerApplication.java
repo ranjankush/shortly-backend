2122051c4afa5596487e099da693d63e75824e65
Ranjan Kumar <ranjankr916264@gmail.com>
2025-07-28 20:23:46 +0530

    cors updated

--- src/main/java/com/url/url_shortner/UrlShortnerApplication.java
+++ src/main/java/com/url/url_shortner/UrlShortnerApplication.java
@@ -1,10 +1,7 @@
 package com.url.url_shortner;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class UrlShortnerApplication {
