1da33257387bfa82b7de82ac880c1f06a3200c05
Ranjan Kumar <ranjankr916264@gmail.com>
2025-07-28 13:13:19 +0530

    .env file added

--- src/main/java/com/url/url_shortner/UrlShortnerApplication.java
+++ src/main/java/com/url/url_shortner/UrlShortnerApplication.java
@@ -1,7 +1,10 @@
 package com.url.url_shortner;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class UrlShortnerApplication {
